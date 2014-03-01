b15be69a4e26bf34d92388e179ae2b3623496b95
Lee Worden <worden.lee@gmail.com>
2014-02-28 17:47:37 -0800

    bayesian large-neighborhood bug

--- batch.pl
+++ batch.pl
@@ -14,6 +14,9 @@ my @nblist = (4,12);
 my @rulelist = ('bayesian-closure-2', 'bayesian-closure-3', 'bayesian-closure-4', 'bayesian-closure-10', 'bayesian-same-neighborhood', 'bayesian');
 my @experiments = ("50x50");
 
+# boolean for keeping track of whether we're simulating on a lattice
+my $lattice = 1;
+
 if (grep(/^--quick$/,@ARGV))
 { $reps = 1; 
   $pstep = 0.05;
@@ -28,7 +31,7 @@ if (grep(/^--regular-size$/,@ARGV) or grep(/^--regular-size-100$/,@ARGV))
   #@nblist = (2,4,6,8,10,14,17,20,24,27,30,27,30,34);
   @nblist = (2,5,8,11,14,17,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300);
   @prange = (0.55, 0.55+$pstep/2);
-  #@experiments = ("50x50","100x100");
+  $lattice = 0;
   if (grep(/^--regular-size-100$/,@ARGV))
   { @experiments = ("100x100"); 
     $batchname = "regular-size-100";
@@ -40,6 +43,7 @@ elsif (grep(/^--regular$/,@ARGV))
 { $batchname = "regular"; 
   $batchargs = " -f regular.settings"; # --n_vertices=2500";
   @nblist = (4,12,50);
+  $lattice = 0;
 }
 elsif (grep(/^--lattice-size$/,@ARGV) or grep(/^--lattice-size-100$/,@ARGV))
 { $batchname = "lattice-size";
@@ -64,13 +68,15 @@ elsif (grep(/--figure1/,@ARGV)) # :)
   if (grep(/--figure1-regular$/,@ARGV))
   { $batchname = 'figure1-regular';
     $batchargs = ' -f regular.settings';
+    $lattice = 0;
   }
 }
 elsif (grep(/^--test$/,@ARGV))
 { $batchname = 'test';
   @nblist = (4, 8);
+  @nblist = (4,8,12,24,40,48,60,80,84,120,112,144,168,180,220,224,288,360,440);
   @prange = (0.55, 0.55+$pstep/2);
-  @rulelist = ('bayesian-closure-2', 'bayesian-closure-3');
+  @rulelist = ('bayesian', 'bayesian-closure-2', 'bayesian-closure-3');
   $reps = 1;
 }
 else # if (grep(/^--lattice$/,@ARGV))
@@ -114,16 +120,18 @@ for my $rule (@rulelist)
     for my $c (1 .. 20)
     { my $tnr = $c*$c + ($c+1)*($c+1) - 1;
       my $inr = (2*$c+1)*(2*$c+1) - 1;
-      print STDERR "$tnr, $inr\n";
+      #print STDERR "$tnr, $inr\n";
       if ( $nb == $tnr )
       { $nr = $c; $metric = 'taxicab'; last; }
       elsif ( $nb == $inr )
       { $nr = $c; $metric = 'infinity'; last; }
     }
-    if ( $batchname =~ /^lattice/ and $nr == 0 ) 
+    if ( $lattice and $nr == 0 ) 
     { die "unaccountable neighborhood size $nb"; }
-    if ($batchname =~ /^lattice/ and $rule eq "bayesian" and $nb > 120) 
-    { die "excessive neighborhood size for lattice $nb"; }
+    if ($lattice and $rule eq "bayesian" and $nb > 50)
+    { warn "lattice neighborhood size $nb is too large for full bayesian simulation"; 
+      next;
+    }
     for my $i (1 .. $reps)
     { for my $p (@plist)
       { for my $experiment (@experiments)
@@ -148,7 +156,8 @@ for my $rule (@rulelist)
             . $batchargs . join("", map {" --$_" } @extra_args)
             . " --outputDirectory=$tmpout";
           print "$comm\n";
-          system($comm) == 0 or die "error running $comm";
+	  system($comm) == 0 or die "error running $comm";
+
           ## @@ !! double check this is the right file to check for existence !!
           if (-e "$tmpout/settings.csv")
           { if ($keep)
